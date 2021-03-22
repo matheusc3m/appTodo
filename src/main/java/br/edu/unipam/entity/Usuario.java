ad50ffe932903ee6d32f01cc7afe22338f33ab82
matheusc3m <matheusfernandes3m@hotmail.com>
2021-03-22 17:33:32 -0300

    implementação da camada de service

--- src/main/java/br/edu/unipam/entity/Usuario.java
+++ src/main/java/br/edu/unipam/entity/Usuario.java
@@ -12,6 +12,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 
 /**
@@ -19,13 +20,16 @@ import javax.persistence.OneToMany;
  * @author josereis
  */
 @Entity
+@NamedQuery(name =  Usuario.GET_ALL_USERS, query = "select u from Usuario u order by u.nome")
 public class Usuario extends AbstractEntity {
 
+    public static final String GET_ALL_USERS = "Usuario.getAllUsers";
+    
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String nome;
-    private String Email;
+    private String email;
 
     @OneToMany(mappedBy = "usuario")
     private Collection<Tarefa> tarefas;
@@ -39,21 +43,13 @@ public class Usuario extends AbstractEntity {
     }
 
     public String getEmail() {
-        return Email;
+        return email;
     }
 
     public void setEmail(String Email) {
-        this.Email = Email;
+        this.email = Email;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-    
-    
+   
     
 }
--- src/main/java/br/edu/unipam/service/UsuarioService.java
+++ src/main/java/br/edu/unipam/service/UsuarioService.java
@@ -6,24 +6,57 @@
 package br.edu.unipam.service;
 
 import br.edu.unipam.entity.Usuario;
+import java.util.List;
+import javax.enterprise.context.RequestScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 /**
  *
  * @author josereis
  */
+
 public class UsuarioService {
 
     @PersistenceContext(name = "pu_todo")
     private EntityManager entityManager;
 
+    //inserir
     public Usuario salvarUsuario(Usuario usuario) {
         entityManager.persist(usuario);
         return usuario;
     }
-    
+
+    //Encontrar usuário por ID
+    public Usuario localizarPorId(Long id) {
+        Usuario userBd = entityManager.find(Usuario.class, id);
+        return userBd;
+    }
+
+    //Remover
+    public void remover(Long id) {
+        Usuario user = localizarPorId(id);
+        if (user != null) {
+            entityManager.remove(user);
+        }
+    }
+
+    //Editar
+    public Usuario editar(Usuario usuario) {
+        Usuario userBd = localizarPorId(usuario.getId());
+
+        if (userBd != null) {
+            entityManager.merge(usuario);
+            return usuario;
+        }
+        return null;
+    }
+
     //Listar Todos os usuários
-    
-    //
+    public List<Usuario> listarTodos ()
+    {
+        //return entityManager.createQuery("select u from Usuario u order by u.nome", Usuario.class).getResultList();
+        return entityManager.createNamedQuery(Usuario.GET_ALL_USERS, Usuario.class).getResultList();
+    }
 }
